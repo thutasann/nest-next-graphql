81cc1ed471812dc1a53d58b9926373811b39248c
Thuta San <thutasann2002@gmail.com>
2023-07-18 10:15:00 +0630

    feat: graphql schemas

--- backend/src/graphql.ts
+++ backend/src/graphql.ts
@@ -1,4 +1,3 @@
-
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -9,29 +8,52 @@
 /* eslint-disable */
 
 export class CreateDonationInput {
-    exampleField?: Nullable<number>;
+  id: number;
+  count: number;
+  displayName: string;
+  email: string;
+  mobile: string;
+  team?: Nullable<string>;
+  message?: Nullable<string>;
+  createdAt?: Nullable<DateTime>;
 }
 
 export class UpdateDonationInput {
-    id: number;
+  id: number;
 }
 
 export class Donation {
-    exampleField?: Nullable<number>;
+  id: number;
+  count: number;
+  displayName: string;
+  email: string;
+  mobile: string;
+  team?: Nullable<string>;
+  message?: Nullable<string>;
+  createdAt?: Nullable<DateTime>;
 }
 
 export abstract class IQuery {
-    abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
+  abstract donations(): Nullable<Donation>[] | Promise<Nullable<Donation>[]>;
 
-    abstract donation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
+  abstract donation(
+    id: number,
+  ): Nullable<Donation> | Promise<Nullable<Donation>>;
 }
 
 export abstract class IMutation {
-    abstract createDonation(createDonationInput: CreateDonationInput): Donation | Promise<Donation>;
+  abstract createDonation(
+    createDonationInput: CreateDonationInput,
+  ): Donation | Promise<Donation>;
 
-    abstract updateDonation(updateDonationInput: UpdateDonationInput): Donation | Promise<Donation>;
+  abstract updateDonation(
+    updateDonationInput: UpdateDonationInput,
+  ): Donation | Promise<Donation>;
 
-    abstract removeDonation(id: number): Nullable<Donation> | Promise<Nullable<Donation>>;
+  abstract removeDonation(
+    id: number,
+  ): Nullable<Donation> | Promise<Nullable<Donation>>;
 }
 
+export type DateTime = any;
 type Nullable<T> = T | null;
